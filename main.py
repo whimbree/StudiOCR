d446014210892114ab384aaf5ba2dfe0f3705c24
JacobSparks23 <jakesparks@Jake-Laptop.localdomain>
2020-06-23 13:48:27 -0400

    stashing

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from PySide2.QtCore import Slot, Qt
+from PySide2.QtCore import Slot, Qt, QSize
 import sys
+import os
+import glob
 import random
-from PySide2.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog
+from PySide2.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QFileDialog, QMainWindow, QLineEdit
+from PySide2.QtWidgets import QGridLayout
 from PySide2.QtGui import QPixmap
+from PIL import Image
 
 # References
 # https://doc.qt.io/qtforpython/
@@ -11,35 +15,56 @@ from PySide2.QtGui import QPixmap
 def main():
     app = QApplication(sys.argv)  # Create application
 
-    widget = Main_UI()  # Create widget
-    widget.show()  # Show widget
+    widget = Notes_Grid()  # Create widget
 
-    app.exec_()  # Start application
+    widget.show()
 
-    exit(0)
+    sys.exit(app.exec_())  # Start application
 
-class Main_UI(QWidget):
+class Main_UI(QMainWindow):
     def __init__(self):
-        QWidget.__init__(self)
+        QMainWindow.__init__(self)
 
-        self.choose_file_button = QPushButton("Choose file")
+        self.setMinimumSize(QSize(300, 200))
+        self.setStyleSheet('background-color : white;')
+        self.choose_file_button = QPushButton("Choose file", self)
+        self.choose_file_button.setStyleSheet('background-color: orange; color: black;')
+        self.choose_file_button.move(900, 360)
 
-        self.label = QLabel(self);
-        pixmap = QPixmap('test_img/conv_props.jpg')
-        self.label.setPixmap(pixmap.scaled(256, 256, Qt.KeepAspectRatio))
-        self.label.show()
+        self.search_bar = QLineEdit(self)
+        self.search_bar.move(80, 20)
+        self.search_bar.resize(200, 32)
 
-        self.layout = QVBoxLayout()
-        self.resize(300, 300)
-        self.move(300, 300)
+        self.search_bar.textChanged.connect(self.update_filter)
+
+        # layout = QGridLayout(self)
+        #
+        # labels = []
+        # count = 0
+        # for file in os.listdir('test_img'):
+        #     if (file.endswith('.jpg')):
+        #         count += 1
+        #         label = QLabel()
+        #         pmap = QPixmap(file)
+        #         pmap = pmap.scaled(150, 150, Qt.KeepAspectRatio, Qt.FastTransformation)
+        #         label.setPixmap(pmap)
+        #         labels.append(label)
+        #
+        # x = 0
+        # y = 0
+        # for label in labels:
+        #     layout.addWidget(label, x, y)
+        #     y += 1
+        #
+        # widget = QWidget()
+        # widget.setLayout(layout)
+        # self.setCentralWidget(widget)
+
         self.setWindowTitle("StudiOCR")
-
-        self.layout.addWidget(self.choose_file_button, alignment=Qt.AlignRight)
-
-        self.setLayout(self.layout)
 
         self.choose_file_button.clicked.connect(self.choose_file)
 
+        self.showFullScreen()
 
     def choose_file(self):
         print("Button pressed")
@@ -47,57 +72,43 @@ class Main_UI(QWidget):
         filepath = file_name[0]
         print(filepath)
 
+    def update_filter(self):
+        print(self.search_bar.text())
 
-class file_browser(QWidget):
+class Notes_Grid(QWidget):
     def __init__(self):
         QWidget.__init__(self)
 
-        self.button = QPushButton("Choose file")
+        # print(glob.glob('test_img/*.jpg'))
+        #
+        self.img1 = QPixmap('test_img/conv_props.jpg')
+        self.img1 = self.img1.scaled(150, 150, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        # self.img2 = QPixmap('test_img/handwritten_notes.jpg')
+        # self.img2 = self.img2.scaled(150, 150, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        #
+        self.label1 = QLabel()
+        self.label1.setPixmap(self.img1)
+        #
+        # self.label2 = QLabel()
+        # self.label2.setPixmap(self.img2)
+        #
+        # self.grid = QGridLayout()
+        # self.grid.addWidget(self.label1, 0, 0)
+        # self.grid.addWidget(self.label2, 0, 1)
+        #
+        # self.setLayout(self.grid)
 
-        # Create and setup layout object
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.button)
+        self.vbox = QVBoxLayout(self)
+        self.vbox.addWidget(self.label1)
 
-        # Apply layout
-        self.setLayout(self.layout)
+        self.button = QPushButton('conv_props')
+        self.vbox.addWidget(self.button)
 
-        # Connecting the signal from button
-        self.button.clicked.connect(self.button_action)
+        self.setLayout(self.vbox)
 
-    def button_action(self):
-        print("Button pressed")
-        self.open_fs()
+        self.setWindowTitle("Photo Grid")
 
-    def open_fs(self):
-        file = QFileDialog.getOpenFileName()
-        filepath = file[0]
-        print(filepath)
-
-
-
-class HelloWidget(QWidget):
-    def __init__(self):
-        QWidget.__init__(self)
-
-        self.hello = ["Hello, world!", "Goodbye, world!"]
-
-        self.button = QPushButton("Click me!")
-        self.text = QLabel(self.hello[0])
-        self.text.setAlignment(Qt.AlignCenter)
-
-        # Create and setup layout object
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-
-        # Apply layout
-        self.setLayout(self.layout)
-
-        # Connecting the signal from button
-        self.button.clicked.connect(self.button_action)
-
-    def button_action(self):
-        self.text.setText(random.choice(self.hello))
+    def show_image(self):
 
 
 if __name__ == "__main__":
